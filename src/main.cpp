69fde96f2f462acaf7933a2c1f022a5d2bb5318f
lyanna-stark <nymphadora.96@gmail.com>
2018-12-24 15:35:05 +0100

    added on_reshape func

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <GL/glut.h>
 #include <iostream>
 
+static void on_reshape(int width, int height);
+
+
 int main(int argc, char** argv){
 	//inicijalizujemo glut
 	glutInit(&argc, argv);
@@ -12,6 +15,10 @@ int main(int argc, char** argv){
 	glutCreateWindow(argv[0]);
 	
 	
+    glutReshapeFunc(on_reshape);
+
+    
+    
 	glClearColor(0, 0, 0, 0);
     glEnable(GL_DEPTH_TEST);    
 	glEnable(GL_NORMALIZE);
@@ -22,3 +29,15 @@ int main(int argc, char** argv){
 	glutMainLoop();
 	return 0;
 }
+
+void on_reshape(int width, int height) {
+	//viewport
+	glViewport(0, 0, width, height);
+
+	//perspektiva tj odsecanje
+	glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(50, (float) width / height, 1, 1500);
+}
+
+
